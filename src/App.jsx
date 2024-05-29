3db907296d2e733907e5f31e1fbceeb5e3e34bc2
unknown <tnwl9540@gmail.com>
2024-05-29 20:29:39 +0900

    0829

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import List from "./components/List";
 
 // TODO: 콘솔창을 확인해보고 input 타이핑할 때 마다
@@ -17,9 +17,15 @@ const App = () => {
     setInput("");
   };
 
-  const filteredItems = items.filter((item) =>
-    item.toLocaleLowerCase().includes("item"),
-  );
+  // const filteredItems = items.filter((item) =>
+  //   item.toLocaleLowerCase().includes("item")
+  // );
+
+  const filteredItems = useMemo(() => {
+    return items.filter((item) => item.toLocaleLowerCase().includes("item"));
+  }, [items]);
+
+  // const newFilteredItems = useMemo(() => filteredItems, [items]);
 
   return (
     <div>
